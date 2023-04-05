9cc7fd0bb7ef83d9f6350183c55e6dd76fed13b6
緋 <40637516+MashiroSA@users.noreply.github.com>
2023-04-05 19:50:24 +0800

    fix(pytest): update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,8 +3,8 @@ from unittest import SkipTest, TestCase
 
 class TestMain(TestCase):
     def test_import(self):
-        from sekai.Voice import Voice
-        from logger.LogManager import LogManager
+        from sekai.voice import Voice
+        from logger.log_manager import LogManager
         from selenium import webdriver
         from selenium.webdriver.chrome.options import Options
         from sekai import config_path, config
